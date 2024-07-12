11d374094275d8438a07e56141bbd3ef3de98955
iniadittt <adityabayu861@gmail.com>
2024-07-12 13:39:24 +0700

    first commit

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -11,7 +11,6 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import LoginForm from "@/components/client/login-form";
-import Image from "next/image";
 import Link from "next/link";
 
 const Page = async ({
@@ -27,16 +26,6 @@ const Page = async ({
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-4 px-4 py-8 md:gap-8 lg:py-24 xl:gap-12">
-      <Link href="/">
-        <Image
-          src="/logo.png"
-          width="120"
-          height="120"
-          alt="Hero"
-          className="mx-auto aspect-square overflow-hidden rounded-full object-cover"
-          priority={true}
-        />
-      </Link>
       <div className="flex flex-col gap-2 xl:gap-4">
         <Card className="w-full max-w-sm">
           <CardHeader>
